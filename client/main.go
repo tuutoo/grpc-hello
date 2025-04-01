bbdc08ee4a43b51f2d71c4eee02b935795a07c7e
tuutoo <tuutoo@gmail.com>
2025-04-01 15:55:01 +0800

    feat: allow secure and insecure for client tests

--- client/main.go
+++ client/main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"flag"
 	"io"
 	"log"
 	"time"
 
 	pb "github.com/tuutoo/grpc-hello/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("192.168.66.203:50051", grpc.WithInsecure())
+	// 命令行参数
+	address := flag.String("addr", "localhost:50051", "gRPC server address")
+	insecureConn := flag.Bool("insecure", false, "Use plaintext (non-TLS) connection")
+	name := flag.String("name", "Jerry", "Name to greet")
+	flag.Parse()
+
+	// 设置连接选项
+	var opts grpc.DialOption
+	if *insecureConn {
+		log.Println("Using insecure connection")
+		opts = grpc.WithTransportCredentials(insecure.NewCredentials())
+	} else {
+		log.Println("Using TLS connection (InsecureSkipVerify=true)")
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true, // 用于测试，生产应验证证书
+		}
+		opts = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
+	}
+
+	conn, err := grpc.Dial(*address, opts)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -22,7 +45,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	stream, err := client.SayHello(ctx, &pb.HelloRequest{Name: "Jerry"})
+	stream, err := client.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
